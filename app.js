c0a2f245f408a300d22b206fbd92619421e587ac
Ashwin Goel <ash@pop-os.localdomain>
2019-12-27 00:57:24 +0530

    cors added

--- app.js
+++ app.js
@@ -6,22 +6,15 @@ var morgan = require('morgan')
 var cookies = require("cookie-parser");
 var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
 
+app.use(cors())
 
 app.use(cookies());
 
 dotenv.config()
 
 app.use(morgan('dev'))
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://legalco.tech");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST");
-    res.header("Access-Control-Allow-Credentials", "true");
-
-    next();
-  });
 
 
 //Connect to Mongodb
