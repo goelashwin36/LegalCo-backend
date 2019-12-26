b251c5b14529208b3725e980a5ee2a3ffaca6dda
Ashwin Goel <ash@pop-os.localdomain>
2019-12-27 00:20:44 +0530

    Authentication now done using headers

--- routes/customerRoutes/verifyCustomerToken.js
+++ routes/customerRoutes/verifyCustomerToken.js
@@ -2,22 +2,19 @@ const jwt = require('jsonwebtoken')
 
 
 module.exports = function (req, res, next) {
-    // console.log(req.headers.cookie)
-    // console.log(cookies.get('token'));
-    // console.log(req)
-    const token = req.cookies.token
-    console.log(token)
-
-    if (!token) {
-        res.status(401).send("Access Denied")
-    }
 
     try {
+        if (req.header("Authorization") === undefined) {
+            throw new Error("Unauthorized")
+        }
+        const token = req.header("Authorization").replace("Bearer ", "")
         const verified = jwt.verify(token, process.env.USER_TOKEN_SECRET)
         req.user = verified._id
         next()
-    }
-    catch (err) {
+
+    } catch (err) {
         res.status(400).send("Invalid Token")
+
     }
+
 }
